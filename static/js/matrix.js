9dc73e2fb230838de605fc064e36300c61c30615
Nick VanCise <nickvancise@MacBook-Pro.local>
2022-11-13 15:25:49 -0800

    periodic update: csp, accessibilty
    - update deps (jquery, bootstrap)
    
    - add csp
    
    - fix accessibility items
    
    - minimize largest images

--- static/js/matrix.js
+++ static/js/matrix.js
@@ -1,63 +1,67 @@
-function genMatrix(){
+function genMatrix() {
   //(base skeleton by Boujjou Achraf)
   var c = document.getElementById("c");
   var ctx = c.getContext("2d");
 
-  var matrix = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789诶比西迪伊艾弗吉艾尺艾杰开艾勒艾马艾娜哦屁吉吾艾儿艾一，@#$%^&*()*&^%+-/~{[|`]}".split("");;
+  var matrix =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789诶比西迪伊艾弗吉艾尺艾杰开艾勒艾马艾娜哦屁吉吾艾儿艾一，@#$%^&*()*&^%+-/~{[|`]}".split(
+      ""
+    );
   var font_size = 12;
-  var columns = c.width/font_size; //number of columns
-  var rows = c.height/font_size;
+  var columns = c.width / font_size; //number of columns
+  var rows = c.height / font_size;
   var columns_old = columns;
   //an array of drops - one per column
   var drops = [];
   //x coordinate
-  for(var x = 0; x < columns; x++)
-      drops[x] = Math.floor((Math.random()*rows)+1);//random starting place within screen rows
+  for (var x = 0; x < columns; x++)
+    drops[x] = Math.floor(Math.random() * rows + 1); //random starting place within screen rows
 
   //draw the characters
-  function drawmatrix()
-  {
-      if(!isScrolledIntoView(c)) {return;} //lazy render
-      
-      if (c.height != window.innerHeight || c.width != window.innerWidth){ //adjust dynamically if width or height has changed
-        c.height = window.innerHeight;
-        c.width = window.innerWidth;
+  function drawmatrix() {
+    if (!isScrolledIntoView(c)) {
+      return;
+    } //lazy render
 
-        columns = c.width/font_size;
-        rows = c.height/font_size;
-        if (columns > columns_old){
-          drops=[]
-          for(var x = 0; x < columns; x++)
-            drops[x] = Math.floor((Math.random()*rows+1));
-        }
-        columns_old=columns
+    if (c.height != window.innerHeight || c.width != window.innerWidth) {
+      //adjust dynamically if width or height has changed
+      c.height = window.innerHeight;
+      c.width = window.innerWidth;
+
+      columns = c.width / font_size;
+      rows = c.height / font_size;
+      if (columns > columns_old) {
+        drops = [];
+        for (var x = 0; x < columns; x++)
+          drops[x] = Math.floor(Math.random() * rows + 1);
       }
+      columns_old = columns;
+    }
 
-      //Black BG for the canvas
-      //translucent BG to show trail
-      ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
-      ctx.fillRect(0, 0, c.width, c.height);
+    //Black BG for the canvas
+    //translucent BG to show trail
+    ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
+    ctx.fillRect(0, 0, c.width, c.height);
 
-      ctx.fillStyle = 'hsl(' + 360 * Math.random() + ', 90%, 50%)';//multi color saturated text
-      ctx.font = font_size + "px arial";
+    ctx.fillStyle = "hsl(" + 360 * Math.random() + ", 90%, 50%)"; //multi color saturated text
+    ctx.font = font_size + "px arial";
 
-      for(var i = 0; i < columns; i++)
-      {
-          var text = matrix[Math.floor(Math.random()*matrix.length)];
-          //x = i*font_size, y = value of drops[i]*font_size
-          ctx.fillText(text, i*font_size, drops[i]*font_size);
+    for (var i = 0; i < columns; i++) {
+      var text = matrix[Math.floor(Math.random() * matrix.length)];
+      //x = i*font_size, y = value of drops[i]*font_size
+      ctx.fillText(text, i * font_size, drops[i] * font_size);
 
-          //sending the drop back to the top randomly after it has crossed the screen
-          //adding a randomness to the reset to make the drops scattered on the Y axis
-          if(drops[i]*font_size > c.height && Math.random() > 0.977)
-              drops[i] = 0;
+      //sending the drop back to the top randomly after it has crossed the screen
+      //adding a randomness to the reset to make the drops scattered on the Y axis
+      if (drops[i] * font_size > c.height && Math.random() > 0.977)
+        drops[i] = 0;
 
-          //incrementing Y coordinate
-          drops[i]+=1;
-      }
+      //incrementing Y coordinate
+      drops[i] += 1;
+    }
   }
 
   setInterval(drawmatrix, 75);
 }
 
-genMatrix()
+genMatrix();
--- static/js/personal.js
+++ static/js/personal.js
@@ -1,101 +1,51 @@
 function isScrolledIntoView(el) {
-    var rect = el.getBoundingClientRect();
-    var elemTop = rect.top-49;
-    var elemBottom = rect.bottom-49;
+  var rect = el.getBoundingClientRect();
+  var elemTop = rect.top - 49;
+  var elemBottom = rect.bottom - 49;
 
-    // Only completely visible elements return true:
-    //var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
-    // Partially visible elements return true:
-    isVisible = elemTop < window.innerHeight && elemBottom >= 0;
-    return isVisible;
+  // Only completely visible elements return true:
+  //var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+  // Partially visible elements return true:
+  isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+  return isVisible;
 }
 
 //used for non canvas elements
-function isElementInViewport (el) {
-    var rect = el[0].getBoundingClientRect();
-    var elemTop = rect.top;
-    var elemBottom = rect.bottom;
+function isElementInViewport(el) {
+  var rect = el[0].getBoundingClientRect();
+  var elemTop = rect.top;
+  var elemBottom = rect.bottom;
 
-    // Only completely visible elements return true:
-    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
-    // Partially visible elements return true:
-    //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
-    return isVisible;
+  // Only completely visible elements return true:
+  var isVisible = elemTop >= 0 && elemBottom <= window.innerHeight;
+  // Partially visible elements return true:
+  //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+  return isVisible;
 }
 // Check if it's time to start the animation
 function checkAnimation() {
-    var $elem = $('#skills-list');
+  var $elem = $("#skills-list");
 
-    // If the animation has already been started
-    if ($elem.hasClass('faded')) return;
+  // If the animation has already been started
+  if ($elem.hasClass("faded")) return;
 
-    if (isElementInViewport($elem)) {
-        // Start the animation
-        $elem.addClass('faded')
-        var basetime = 300;
-        var vartime = 1700;
+  if (isElementInViewport($elem)) {
+    // Start the animation
+    $elem.addClass("faded");
+    var basetime = 300;
+    var vartime = 1700;
 
-        $elem.children().each((index, child) => {
-            setTimeout( function(){ $(child).addClass('fadeIn');}, basetime+index*50)
-        });
-    }
+    $elem.children().each((index, child) => {
+      setTimeout(function () {
+        $(child).addClass("fadeIn");
+      }, basetime + index * 50);
+    });
+  }
 }
 
 // Capture scroll events
-$(window).scroll(function(){
-    checkAnimation();
-});
-
-var scrolled = 0;
-var currentelemcount = 0;
-$("#projbutton-l").on("click" ,function(){
-    if($(".section-3 > .container > .row > .col-lg-8 > #projbutton-r").is(":hidden")){
-        $(".section-3 > .container  > .row > .col-lg-8 > #projbutton-r").fadeIn();
-    }
-
-    if($(".projects-list").width()>=600){
-        scrolled-=(2*($(".tile").width()+25));
-        currentelemcount-=4;
-    } else{
-        scrolled-=($(".tile").width()+25);
-        currentelemcount-=2;
-    }
-
-    if(currentelemcount==0){
-      $(".section-3 > .container > .row > .col-lg-8 > #projbutton-l").fadeOut();
-    }
-
-    $(".projects-list").animate({
-        scrollLeft:  scrolled
-    });
-});
-
-$("#projbutton-r").on("click" ,function(){
-    var elemcount = $(".projects-list > .tile").length;
-    var eleminc,scrollinc = 0;
-
-    if($(".section-3 > .container > .row > .col-lg-8 > #projbutton-l").is(":hidden")){
-        $(".section-3 > .container  > .row > .col-lg-8 > #projbutton-l").fadeIn();
-    }
-
-    if($(".projects-list").width()>=600){
-        eleminc = 4;
-        scrollinc = (2*($(".tile").width()+25));
-    } else{
-        eleminc = 2;
-        scrollinc = $(".tile").width()+25;
-    }
-
-    if(currentelemcount+eleminc >= elemcount-eleminc){
-        $(".section-3 > .container  > .row > .col-lg-8 > #projbutton-r").fadeOut();
-    }
-
-    scrolled += scrollinc;
-    currentelemcount += eleminc;
-
-    $(".projects-list").animate({
-        scrollLeft:  scrolled
-    });
+$(window).scroll(function () {
+  checkAnimation();
 });
 
 function popup(mylink, windowname) {
@@ -107,19 +57,94 @@ function popup(mylink, windowname) {
   return false;
 }
 
-function displayIndustryDescription(elem){
-  console.log("displaying industry descrip")
-  $('.popup-content > p').text(industryprojects[$(elem).attr('arraylink')]);
-  $('.popup-content > h2').text($(elem).attr('arraylink'));
+let industryprojects = {
+  "Airflow Ingestion Cluster":
+    "This environment consists of a distributed Apache Airflow Cluster, backed by Celery, that utilizes an array of custom docker containers to fetch and process data from public webpages and data buckets, and send this processed data to the Ingestion API.\n\nDocker was used to isolate browser reliant code, and build reusable, language agnostic, one-time use containers. The containers are designed to allow for fast and reliable setup, deployment, testing, tear down, cloning, and failure recovery.",
+  "Real-Time Ingestion API":
+    "The Ingestion API is designed to function as the middle man between independent user programs orchestrated by Apache Airflow that collect data, and multiple postgres database connections.\n\nThis API was built with golang, and utilizes GORM and Gorilla Mux for database operations and routing, and can concurrently process multipart forms in bulk for data ingestion through http requests.",
+  "Ingestion Index Crawler":
+    "This crawler is designed to reduce search times of already ingested data for an in-house manual data ingestion system. It crawls all the pages of existing instances for a specified user, and produces a list with the name, page number, and status of each instance.\n\nThis allows for fast searching and navigation using the built in utility script. The crawler was built using Selenium in python and encapsulated in a docker container for easy deployment.",
+};
+
+function displayIndustryDescription(elem) {
+  $(".popup-content > p").text(industryprojects[$(elem).attr("arraylink")]);
+  $(".popup-content > h2").text($(elem).attr("arraylink"));
   $(".popup-overlay, .popup-content").fadeIn();
-  $('body').css('overflow','hidden')
+  $("body").css("overflow", "hidden");
 }
 
-$(".close, .popup-overlay").on("click", function() {
-  $(".popup-overlay, .popup-content").fadeOut();
-  $('body').css('overflow','auto')
+//click handlers
+var scrolled = 0;
+var currentelemcount = 0;
+$("#projbutton-l").on("click", function () {
+  if (
+    $(".section-3 > .container > .row > .col-lg-8 > #projbutton-r").is(
+      ":hidden"
+    )
+  ) {
+    $(".section-3 > .container  > .row > .col-lg-8 > #projbutton-r").fadeIn();
+  }
+
+  if ($(".projects-list").width() >= 600) {
+    scrolled -= 2 * ($(".tile").width() + 25);
+    currentelemcount -= 4;
+  } else {
+    scrolled -= $(".tile").width() + 25;
+    currentelemcount -= 2;
+  }
+
+  if (currentelemcount == 0) {
+    $(".section-3 > .container > .row > .col-lg-8 > #projbutton-l").fadeOut();
+  }
+
+  $(".projects-list").animate({
+    scrollLeft: scrolled,
+  });
 });
 
-//click handlers
-$('.industrylink').click(function(){ displayIndustryDescription($(this)); return false; });
+$("#projbutton-r").on("click", function () {
+  var elemcount = $(".projects-list > li > .tile").length;
+  var eleminc,
+    scrollinc = 0;
 
+  if (
+    $(".section-3 > .container > .row > .col-lg-8 > #projbutton-l").is(
+      ":hidden"
+    )
+  ) {
+    $(".section-3 > .container  > .row > .col-lg-8 > #projbutton-l").fadeIn();
+  }
+
+  if ($(".projects-list").width() >= 600) {
+    eleminc = 4;
+    scrollinc = 2 * ($(".tile").width() + 25);
+  } else {
+    eleminc = 2;
+    scrollinc = $(".tile").width() + 25;
+  }
+
+  if (currentelemcount + eleminc >= elemcount - eleminc) {
+    $(".section-3 > .container  > .row > .col-lg-8 > #projbutton-r").fadeOut();
+  }
+
+  scrolled += scrollinc;
+  currentelemcount += eleminc;
+
+  $(".projects-list").animate({
+    scrollLeft: scrolled,
+  });
+});
+
+$(".close, .popup-overlay").on("click", function () {
+  $(".popup-overlay, .popup-content").fadeOut();
+  $("body").css("overflow", "auto");
+});
+
+$(".industrylink").on("click", function () {
+  displayIndustryDescription($(this));
+  return false;
+});
+
+$(".resume-button").on("click", function () {
+  return popup(this, "resume");
+});
