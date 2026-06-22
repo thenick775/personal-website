7bb5722a45ba1f58c57387d57ed3133b70c00f2f
thenick775 <nvancisedev@gmail.com>
2026-06-22 14:16:28 -0700

    chore: more initial wip work

---
+++ eslint.config.ts
@@ -0,0 +1,33 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import eslintPluginAstro from 'eslint-plugin-astro';
+
+export default defineConfig([
+  {
+    ignores: ['dist/**', '.astro/**'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended', tseslint.configs.strictTypeChecked],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
+  ...eslintPluginAstro.configs.recommended,
+  {
+    files: ['**/*.astro'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+        project: './tsconfig.json',
+        extraFileExtensions: ['.astro'],
+      },
+    },
+  },
+]);
--- src/data/site.ts
+++ src/data/site.ts
@@ -14,7 +14,7 @@ export const site = {
       "Currently I'm working on my masters at the University of Nevada Las Vegas, as well as pursuing my dreams as a software engineer.",
       'In addition to this, in my own time I dabble in emulator development, webscraping, data visualization, and infrastructure programming.',
       "When I'm not studying or working on my personal projects I spend my time enjoying nature, reading science fiction, and working on classic vehicles!",
-    ],
+    ] as string[],
   },
   contact: {
     title: 'Contact me',
